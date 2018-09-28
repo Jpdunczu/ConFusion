e1f88c514830d5c6f711d5d09c676d3377312b1d
Joshua Duncan <jd52981@gmail.com>
2018-09-27 23:38:10 -0700

    Assignment 3: reactive form building

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -9,6 +9,11 @@ import { DishService } from '../services/dish.service';
 
 import { switchMap } from 'rxjs/operators';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
+
+import "hammerjs";
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -22,13 +27,36 @@ export class DishdetailComponent implements OnInit {
   prev: number;
   next: number;
 
+  commentForm: FormGroup;
+  newComment: Comment;
+  formErrors = { // simple JS object for error messages
+    // this is defined here because it will be used in onValueChanged method.
+    'author': '',
+    'comment': ''
+  };
+  date: Date;
+
+  validationMessages = {
+    'author': {
+      'required': "Name is required.",
+      'minlength': 'First Name must be at least 2 characters long.',
+      'maxlength': 'First Name cannot be more than 25 characters long.'
+    },
+    'comment': {
+      'required': "Comment is required.",
+      'minlength': 'Comment must be at least 10 characters long.'
+    }
+  };
+
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) { 
+      this.createForm();
+    }
 
   ngOnInit() {
-    this.dishservice.getDishIds()
-      .subscribe(dishIds => this.dishIds = dishIds);
+    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
 
     // fetch the info
     // what is snapshot: taking one snapshot from the route service, obtaining the value of the params at that particular time.
@@ -51,6 +79,50 @@ export class DishdetailComponent implements OnInit {
   goBack(): void {
     // location service has a method to go back
     this.location.back();
+  }
+
+  createForm() {
+    this.commentForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      rating: 5,
+      comment: ['', [Validators.required, Validators.minLength(10)]]
+    });
+
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // reset form validation messages.
+  }
+
+  onSubmit() {
+    this.newComment = this.commentForm.value;
+    console.log(this.newComment);
+    this.date = new Date();
+    this.newComment.date = this.date.toISOString();
+    this.dish.comments.push(this.newComment);
+    this.commentForm.reset({
+      author: '',
+      rating: 5,
+      comment: ''
+    });
+  }
+
+  onValueChanged(data?: any) {
+    if(!this.commentForm) { return; }
+    const form = this.commentForm;
+
+    for(const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if(control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for(const key in control.errors) {
+          this.formErrors[field] += messages[key] + '';
+        }
+      }
+    }
+
+    
   }
 
 }
--- src/app/shared/comment.ts
+++ src/app/shared/comment.ts
@@ -5,3 +5,4 @@ export class Comment {
     date: string;
 }
 
+
