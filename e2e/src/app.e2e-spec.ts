4debc391a1500d5d564e42d400ab5cdc90ddb89e
Joshua Duncan <jd52981@gmail.com>
2018-10-04 00:55:09 -0700

    Angular Build

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -7,8 +8,33 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to conFusion2!');
+  it('should display message saying Ristorante Con Fusion', () => {
+    page.navigateTo('/'); // root of Angular application
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
+  });
+
+  it('should navigate to about us page by clicking on the link', () => {
+    page.navigateTo('/');
+
+    let navlink = page.getAllElements('a')  // get all elements that have the <a> tag.
+      .get(1);  // then specify the second element of that, which is the about us link.
+    navlink.click();  // click on that link
+    expect(page.getParagraphText('h3')).toBe('About Us'); // toBe is Jasmine syntax 
+  });
+
+  it('should enter a new comment for the first dish', () => {
+    page.navigateTo('/dishdetail/0');
+
+    let newAuthor = page.getElement('input[type=text]');
+    newAuthor.sendKeys('Test Author');
+
+    let newComment = page.getElement('textarea');
+    newComment.sendKeys('Test comment is the best comment');
+
+    let newSubmitButton = page.getElement('button[type=submit]');
+    newSubmitButton.click();
+
+    // protractor method
+    browser.pause();
   });
 });
--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,16 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 import { MenuComponent } from './menu.component';
+
+//import { MaterialModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { DISHES } from '../shared/dishes';
+import { baseURL } from '../shared/baseurl';
+import { Observable, of } from 'rxjs';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
+  let dishServiceStub = {
+    // mock getDishes
+    getDishes: function(): Observable<Dish[]> {
+      return of(DISHES);
+    }
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      imports: [
+        BrowserAnimationsModule,
+        MatGridListModule,
+        MatProgressSpinnerModule,
+        FlexLayoutModule,
+        RouterTestingModule.withRoutes([
+          {path: 'menu', component: MenuComponent}
+        ])
+      ],
+      declarations: [ MenuComponent ],
+      providers: [
+        { provide: DishService, useValue: dishServiceStub },
+        { provide: 'BaseURL', useValue: baseURL }
+      ]
     })
     .compileComponents();
+
+    let dishService = TestBed.get(DishService);
+
   }));
 
   beforeEach(() => {
@@ -22,4 +59,28 @@ describe('MenuComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // test to see whether Dishes var inside my menu comp is actually receiving the values or not
+  it('dishes items should be 4', () => {
+    expect(component.dishes.length).toBe(4);
+    expect(component.dishes[1].name).toBe('Zucchipakoda');
+    expect(component.dishes[3].featured).toBeTruthy(); // should fail
+  });
+
+  it('should use dishes in the template', () => {
+    fixture.detectChanges();
+
+    let de: DebugElement;
+    let el: HTMLElement;
+
+    // get acces from the DOM Document object Model
+    // 'By' access to specific elements in the DOM
+    de = fixture.debugElement.query(By.css('h1'));
+    // get access to the element which I specified in 'de'
+    el = de.nativeElement;
+
+    // check if the DISHES[0].name is included in the DOM first h1 element and that it uses the uppercase pipe.
+    expect(el.textContent).toContain(DISHES[0].name.toUpperCase());
+  });
+
 });
